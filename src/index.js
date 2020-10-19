7fbd866dfd7fd89720aedf1636e49669fe6dec41
boldCM <carolin@cc-meier.de>
2020-10-19 20:40:55 +0200

    Clean up for Output Management

--- src/index.js
+++ src/index.js
@@ -1,23 +1,10 @@
 import _ from "lodash";
-import "./style.css";
-import Wasserfall from "./Wasserfall.jpg";
-import Data from "./data.xml";
-import Notes from "./data.csv";
 
 function component() {
   const element = document.createElement("div");
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(["Hello", "webpack"], " ");
-  element.classList.add("hello");
-
-  const myIcon = new Image();
-  myIcon.src = Wasserfall;
-
-  element.appendChild(myIcon);
-
-  console.log(Data);
-  console.log(Notes);
 
   return element;
 }
