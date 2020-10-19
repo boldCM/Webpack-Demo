14d7929240ee8f51324ce2a9364fa8f419e26272
boldCM <carolin@cc-meier.de>
2020-10-19 20:59:53 +0200

    Cleaned dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -9,6 +10,7 @@ module.exports = {
     print: "./src/print.js",
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: "Output Management",
     }),
